292f53c73d4808ed3bb446e60332b63c77459bdd
Avijit Maity <avisrabon22@gmail.com>
2025-01-09 18:21:11 +0530

    first commit for log tracker

---
+++ src/main/java/com/iimi/logtracker/LogtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.iimi.logtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iimi/logtracker/LogtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iimi.logtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
